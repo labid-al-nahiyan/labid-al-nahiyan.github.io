e081ce9fb52de122aaa4e38cab1ff47b2e405246
labid-al-nahiyan <labid.nahiyan12@gmail.com>
2025-01-18 00:52:20 +0600

    random

--- src/app/labid_al_nahiyan/page.tsx
+++ src/app/labid_al_nahiyan/page.tsx
@@ -1,15 +1,11 @@
-'use client';
-
 import Link from "next/link";
-import { redirect } from "next/navigation";
-import { useEffect } from "react";
 
 export default function Labid(){
 
     
-    useEffect(() => {
-        redirect('/')
-    }, []);
+    
 
-    return <div><Link href="https://labid-al-nahiyan.github.io/">Updated Link : </Link>if redirect Does not work (sorry :=)</div>
+    return (
+        <div><Link href="https://labid-al-nahiyan.github.io/">Updated Link : </Link>if redirect Does not work (sorry :=)</div>
+    )
 }
